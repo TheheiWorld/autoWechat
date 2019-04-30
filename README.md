cfb2c5192c8fb5a3d4ecda09f306c352de312cd0
TheheiWorld <juststand0627@yeah.net>
2019-04-30 17:53:46 +0800

    add

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# autoWechat
+### 微信自动回复 hello world
+### 一、依赖
+- Itchat
+- pystaller 
--- com/juststand/auto.py
+++ com/juststand/auto.py
@@ -5,26 +5,43 @@
 # create_date : 2019/4/30 上午10:47
 
 import itchat
-
-print('''
-    关键字回复操作
-    预定默认字，
-''')
+from com.juststand.Color import FontColor
 
 CONTENT = ['开始预定','开始预约','开始约', '开始预','开始订']
-def inputMessage():
-    keyword_booking = str(input('请输入今日预定关键字，按回车键确认，如不输入，直接回车，将采取默认关键字：'))
-
-
-MESSAGE = str(input("请输入今日预定回复内容:"))
-
 CAN_SEND = ['maitaoJavakaifaerzu']
+
+MESSAGE = ''
+
+print(FontColor.green + FontColor.default + '*********** 关键字 ***********' + FontColor.end)
+print(FontColor.red + FontColor.default + '默认回复关键字' + CONTENT.__str__() + FontColor.end )
+
+def inputMessage():
+    global CONTENT
+    global MESSAGE
+
+    # 关键字
+    print(FontColor.green + FontColor.default + "notes: 请逐次输入今日预定关键字，输入 e 代表结束；若没有输入任何关键字，则采取默认关键字" + FontColor.end)
+    CONTENT_TEMP = []
+    while True:
+        keyword_booking = str(input('请输入关键字:'))
+        if keyword_booking == 'e':
+            break
+        if keyword_booking:
+            CONTENT_TEMP.append(keyword_booking)
+    if CONTENT_TEMP.__len__() > 0:
+        CONTENT = CONTENT_TEMP
+
+    ### 回复
+    MESSAGE = str(input('请输入回复内容，按回车键确认：'))
+
+inputMessage()
 
 @itchat.msg_register(itchat.content.TEXT)
 def text_reply(msg):
-    print(MESSAGE)
     username = msg.User.PYQuanPin
     message = msg.Content
+    if username not in CAN_SEND:
+        return
     if message in CONTENT:
         return MESSAGE
     return
@@ -32,8 +49,11 @@ def text_reply(msg):
 @itchat.msg_register(itchat.content.INCOME_MSG, isGroupChat=True)
 def download_files(msg):
     username = msg.User.PYQuanPin
-    if username in CAN_SEND:
-        return msg.text
+    message = msg.Content
+    if username not in CAN_SEND:
+        return
+    if message in CONTENT:
+        return MESSAGE
     return
 
 
