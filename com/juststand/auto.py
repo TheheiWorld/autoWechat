c16254e9ff46a0cf0ceb3e0f70bc37e36ad2cbda
TheheiWorld <juststand0627@yeah.net>
2019-04-30 18:17:48 +0800

    fix

--- com/juststand/auto.py
+++ com/juststand/auto.py
@@ -5,7 +5,39 @@
 # create_date : 2019/4/30 上午10:47
 
 import itchat
-from com.juststand.Color import FontColor
+
+class FontColor():
+    """
+    调用方式：颜色/背景色/+下划线标志 + 需要加颜色的文字 + 结束标志
+    """
+    # 颜色码
+    white = "\033[30;"  # default
+    red = "\033[31;"
+    green = "\033[32;"
+    brown = "\033[33;"
+    Pink = "\033[34;"
+    Violet = "\033[35;"
+    blue = "\033[36;"
+    black = "\033[37;"
+
+    # 背景色
+    white_background = "\033[40;"  # default
+    red_background = "\033[41;"
+    green_background = "\033[42;"
+    brown_background = "\033[43;"
+    Pink_background = "\033[44;"
+    Violet_background = "\033[45;"
+    blue_background = "\033[46;"
+    black_background = "\033[47;"
+
+    # 下划线标志
+    default = "0m"
+    underline = "4m"
+
+    # 结束标志位
+    end = "\033[0m"
+
+    
 
 CONTENT = ['开始预定','开始预约','开始约', '开始预','开始订']
 CAN_SEND = ['maitaoJavakaifaerzu']
