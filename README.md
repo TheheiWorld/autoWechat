e2b1f6aaa5a4a2da4382c53abced12a3c27d967f
TheheiWorld <juststand0627@yeah.net>
2019-05-05 12:32:54 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 ### 微信自动回复 hello world
 ### 一、依赖
 - Itchat
-- pystaller 
+- pyinstaller 
+
+### 二、简介
+微信自动回复，可以针对群／个人自动回复，
+
+#### 2.1 Itchat
+> [中文API](https://itchat.readthedocs.io/zh/latest/)
+
+#### 2.2 pyinstaller
+> pyinstaller -F 指定文件 打包成可执行文件
--- com/juststand/auto.py
+++ com/juststand/auto.py
@@ -7,52 +7,20 @@
 import itchat
 
 
-class FontColor:
-    """
-    调用方式：颜色/背景色/+下划线标志 + 需要加颜色的文字 + 结束标志
-    """
-    # 颜色码
-    white = "\033[30;"
-    red = "\033[31;"
-    green = "\033[32;"
-    brown = "\033[33;"
-    Pink = "\033[34;"
-    Violet = "\033[35;"
-    blue = "\033[36;"
-    black = "\033[37;"
-
-    # 背景色
-    white_background = "\033[40;"
-    red_background = "\033[41;"
-    green_background = "\033[42;"
-    brown_background = "\033[43;"
-    Pink_background = "\033[44;"
-    Violet_background = "\033[45;"
-    blue_background = "\033[46;"
-    black_background = "\033[47;"
-
-    # 下划线标志
-    default = "0m"
-    underline = "4m"
-
-    # 结束标志位
-    end = "\033[0m"
-
-
 CONTENT = ['开始预定', '开始预约', '开始约', '开始预', '开始订']
 CAN_SEND = []
 
 MESSAGE = ''
 
-print(FontColor.green + FontColor.default + '*********** 关键字 ***********' + FontColor.end)
-print(FontColor.red + FontColor.default + '默认回复关键字' + CONTENT.__str__() + FontColor.end)
+print('*********** 关键字 ***********')
+print('默认回复关键字' + CONTENT.__str__())
 
 
 def inputMessage():
     global CONTENT
     global MESSAGE
     # 关键字
-    print(FontColor.green + FontColor.default + "notes: 请逐次输入今日预定关键字，输入 e 代表结束；若没有输入任何关键字，则采取默认关键字" + FontColor.end)
+    print("notes: 请逐次输入今日预定关键字，输入 e 代表结束；若没有输入任何关键字，则采取默认关键字")
     CONTENT_TEMP = []
     while True:
         keyword_booking = str(input('请输入关键字:'))
@@ -69,10 +37,9 @@ def inputMessage():
     # 自动回复人群
     print('')
     print('')
-    print('')
-    print(FontColor.red + FontColor.default + "notes: 请逐次输入需要自动回复的人，输入 e 代表结束；若没有输入，则无法自动回复" + FontColor.end)
+    print("notes: 请逐次输入需要自动回复的人，输入 e 代表结束；若没有输入，则无法自动回复")
     while True:
-        nickname = str(input('请输入名称:'))
+        nickname = str(input('请输入微信昵称:'))
         if nickname == 'e':
             break
         if nickname:
@@ -102,5 +69,5 @@ def group_reply(msg):
     return
 
 
-itchat.auto_login()
+itchat.auto_login(hotReload=True)
 itchat.run()
